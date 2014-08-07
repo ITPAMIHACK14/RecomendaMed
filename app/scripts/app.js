4ef2357020290bebe3cc3c224688ab4b246016f4
Nicolás Passerini <npasserini@gmail.com>
2014-08-07 20:34:24 -0300

    Proyecto Angular con encuestas generadas automáticamente

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularApp
+ * @description
+ * # angularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/encuesta', {
+        templateUrl: 'views/encuestas/familiar.html',
+        controller: 'EncuestaFamiliarController'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularApp
+ */
+angular.module('angularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.nombre='Nico';
+  })
+  .controller('EncuestaFamiliarController', function ($scope) {
+    $scope.nombre='Nico';
+  });
