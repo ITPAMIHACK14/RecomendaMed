fa8a4c19bfba4d93f98e1fe3437c1d895c1d8443
Gustavo Brey <gusbrey@gmail.com>
2014-11-15 15:51:39 -0300

    resultados

--- app/scripts/controllers/resultado.js
+++ app/scripts/controllers/resultado.js
@@ -8,6 +8,19 @@
  * Controller of the angularApp
  */
 angular.module('angularApp')
-  .controller('ResultCtrl', function ($scope) {
-        console.log("Jugar controller: ");
+  .controller('ResultCtrl', function ($scope, $rootScope, $location) {
+
+        $scope.templateUrl = 'views/partials/itemResult.html';
+
+        var items = [{id:1, nombre:"Hospital Italiano", direccion:"Peron 5555"  }, {id:2, nombre:"Los Arcos", direccion:"Juan b Justo 4444"  }, {id:3, nombre:"Cesar Milstein", direccion:"EEUU 4444"  }];
+
+        $scope.items = items;
+
+        $scope.displayItem = function(idItem){
+            console.log("Display " + idItem);
+            $rootScope.itemId = idItem;
+            $location.path('/display');
+
+        };
+
     })
